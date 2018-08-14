a561892a18346baa4ced3dc67043095c50aa2a8a
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2018-08-14 16:19:55 +1000

    Add comments to Main

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,10 @@ import System.IO.Hurl
 import System.Directory
 import System.Environment
 
+--
+-- Check wether the user passed an action in and, if so, 
+-- passing the renaming arguments to the action.
+--
 main :: IO ()
 main =
   do as <- getArgs
@@ -14,28 +18,46 @@ main =
            Nothing -> putStrLn "error: unknown action."
            Just  f -> f xs
 
+--
+-- The action lookup table.
+--
 actionTable :: [(String, ([String] -> IO ()))]
 actionTable = [
   ("new", new)
   ]
 
+--
+-- Create a new directory with the structure of a specified template.
+--
 new       :: [String] -> IO ()
 new [x,y] = do t <- readTemplate x; createDirectory y; runHurl y t
 new _     = putStrLn "error: new: insufficient arguments."
 
+--
+-- Read the contents of a template.
+--
 readTemplate   :: String -> IO String
 readTemplate t =
   do tp <- templatePath t
      readFile tp
 
+--
+-- Build the full path of a template.
+--
 templatePath   :: String -> IO FilePath
 templatePath t =
   do td <- templateDir
      return (td ++ "/" ++ (templateName t))
 
+--
+-- Attach the file extension to the templates name.
+--
 templateName   :: String -> FilePath
 templateName t = t ++ ".hurl"
 
+--
+-- The directory where templates are stored.
+--
 templateDir :: IO FilePath
 templateDir =
   do h <- getHomeDirectory
